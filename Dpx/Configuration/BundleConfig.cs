95d594ae0526e0afe511b9b3e1b3e37c0260e094
diogofriggo <diogofriggobr@gmail.com>
2013-01-22 17:23:05 -0200

    Removed clutter
    Moved files one folder up
    Deleted default clutter that comes with the default MVC template
    Some packages that we probably won't use are to be removed too like
    WebAPI, WebMatrix, OpenAuth etc

---
+++ Dpx/Configuration/BundleConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Dpx
+{
+    public class BundleConfig
+    {
+        public static void Configure(BundleCollection bundles)
+        {
+        }
+    }
+}
